9a3f29681fd75f08b5c5c9303f417b0d6f55082c
kno-be <beknoblauch18@gmail.com>
2021-08-15 03:27:03 -0300

    adjusting environment

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.set(express.static('public'));
+app.use(express.static('public'));
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
