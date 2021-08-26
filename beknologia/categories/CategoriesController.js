a79c98a9c6787d6c7afec3edb7ba8940771ce91b
kno-be <beknoblauch18@gmail.com>
2021-08-26 12:39:35 -0300

    upd: all file

--- beknologia/categories/CategoriesController.js
+++ beknologia/categories/CategoriesController.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const slugify = require("slugify");
 
 const Category = require("./Category")
 
@@ -7,15 +8,23 @@ router.get("/admin/categories/new", (req, res) => {
     res.render("admin/categories/new")
 })
 
+router.get("/admin/categories/list", (req, res) => {
+    res.render("admin/categories/list")
+})
+
 router.post("/admin/categories/new", (req, res) => {
     var name = req.body.name;
-    Category.create({
+    if(name != undefined) {
+        Category.create({
         name:   name,
+        slug: slugify(name)
     }).then(() => {
         res.redirect("/blog");    
     }).catch((err) => {
         console.log(err)
-    })
+    })}else{
+        res.redirect("admin/categories/new")
+    }
 })
 
 module.exports = router;
